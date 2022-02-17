e7e69ca3ff43209c2a554614c70ad1ce26f25b33
a20antchoand <a20antchoand@inspedralbes.cat>
2022-02-17 09:40:41 +0100

    Comit antes de la tormenta

--- app/src/main/java/com/example/testauth/EmployeeActivity.java
+++ app/src/main/java/com/example/testauth/EmployeeActivity.java
@@ -13,11 +13,9 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.errorprone.annotations.Var;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
@@ -29,19 +27,24 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 
 public class EmployeeActivity extends AppCompatActivity {
 
-    static TextView iniciarTextView;
-    static TextView acabarTextView;
-    static TextView resultat;
-    static Button iniciarJornadaBtn;
-    static Button acabarJornadaBtn;
-    static Map<String, Integer> dataEntrada;
-    static Map<String, Integer> dataSortida;
-    static DocumentSnapshot document;
-    static FirebaseFirestore db;
-    static FirebaseUser user;
+    TextView iniciarTextView;
+    TextView acabarTextView;
+    TextView resultat;
+    Button iniciarJornadaBtn;
+    Button acabarJornadaBtn;
+
+    Map<String, Integer> fechaEntrada;
+    Map<String, Integer> fechaSortida;
+
+    HashMap<String, Object> dadesUsuari;
+
+    DocumentSnapshot document;
+    FirebaseFirestore db;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,171 +56,185 @@ public class EmployeeActivity extends AppCompatActivity {
     }
 
     public void setup () {
-        iniciarTextView = ((TextView) findViewById(R.id.iniciarTextView));
-        acabarTextView = ((TextView) findViewById(R.id.acabarTextView));
-        resultat = ((TextView) findViewById(R.id.resultat));
-        iniciarJornadaBtn = ((Button) findViewById(R.id.iniciarJornada));
-        acabarJornadaBtn = ((Button) findViewById(R.id.acabarJornada));
+        iniciarTextView = findViewById(R.id.iniciarTextView);
+        acabarTextView = findViewById(R.id.acabarTextView);
+        resultat = findViewById(R.id.resultat);
+        iniciarJornadaBtn = findViewById(R.id.iniciarJornada);
+        acabarJornadaBtn = findViewById(R.id.acabarJornada);
         db = FirebaseFirestore.getInstance();
         user = FirebaseAuth.getInstance().getCurrentUser();
 
         ((TextView) findViewById(R.id.usuariTextView)).setText(user.getEmail());
 
-        ((Button) findViewById(R.id.logOutEmployee)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        findViewById(R.id.logOutEmployee).setOnClickListener(view -> {
 
-                FirebaseAuth.getInstance().signOut();
-                showAuth();
-            }
+            FirebaseAuth.getInstance().signOut();
+            showAuth();
         });
-
-        validarUsuarioDB(user);
-
-    }
-
-    private void validarUsuarioDB(FirebaseUser user) {
 
         RecuperarUsuariBBDD();
 
     }
 
 
-    private void CrearUsuarioBBDD(FirebaseFirestore db, FirebaseUser user) {
-
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("email", user.getEmail());
-        map.put("hora_entrada", 0);
-        map.put("minuts_entrada", 0);
-        map.put("hora_sortida", 0);
-        map.put("minuts_sortida", 0);
-
-        db.collection("users").document(user.getEmail())
-                .set(map)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Log.d(TAG, "DocumentSnapshot successfully written!");
-
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error writing document", e);
-                        Toast toast2=Toast. makeText(getApplicationContext(),"No sha guardat a la BBDD",Toast. LENGTH_SHORT);
-                        toast2. setMargin(50,50);
-                        toast2. show();
-                    }
-                });
-
-    }
-
     private void RecuperarUsuariBBDD() {
 
 
-        DocumentReference docRef = db.collection("users").document(user.getEmail());
-        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    document = task.getResult();
-                    if (document.exists()) {
-                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
-                        comprovarEntradaSortida();
-                    } else {
-                        Log.d(TAG, "No such document");
-                        Toast toast=Toast. makeText(getApplicationContext(),"Se crea el usuario en la BBDD",Toast. LENGTH_SHORT);
-                        toast. show();
-                        CrearUsuarioBBDD(db, user);
-                    }
+        DocumentReference docRef = db.collection("users").document(Objects.requireNonNull(user.getEmail()));
+        docRef.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                document = task.getResult();
+                if (document.exists()) {
+                    Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                    comprovarEntradaSortida();
                 } else {
-                    Log.d(TAG, "get failed with ", task.getException());
+                    Log.d(TAG, "No such document");
+                    Toast toast=Toast. makeText(getApplicationContext(),"Se crea el usuario en la BBDD",Toast. LENGTH_SHORT);
+                    toast. show();
+                    GuardarUsuarioBBDD();
                 }
+            } else {
+                Log.d(TAG, "get failed with ", task.getException());
             }
-
-
         });
 
     }
 
+    private void GuardarUsuarioBBDD() {
+
+        dadesUsuari = new HashMap<>();
+        dadesUsuari.put("email", user.getEmail());
+        dadesUsuari.put("hora_entrada", 0);
+        dadesUsuari.put("minuts_entrada", 0);
+        dadesUsuari.put("hora_sortida", 0);
+        dadesUsuari.put("minuts_sortida", 0);
+
+        db.collection("users").document(Objects.requireNonNull(user.getEmail()))
+                .set(dadesUsuari)
+                .addOnSuccessListener(aVoid -> Log.d(TAG, "DocumentSnapshot successfully written!"))
+                .addOnFailureListener(e -> {
+                    Log.w(TAG, "Error writing document", e);
+                    Toast toast2=Toast. makeText(getApplicationContext(),"No sha guardat a la BBDD",Toast. LENGTH_SHORT);
+                    toast2. setMargin(50,50);
+                    toast2. show();
+                });
+
+    }
+
+    private void GuardarUsuarioEntradaBBDD(long hora, long minuts) {
+
+        dadesUsuari.put("hora_entrada", hora);
+        dadesUsuari.put("minuts_entrada", minuts);
+
+        db.collection("users").document(Objects.requireNonNull(user.getEmail()))
+                .set(dadesUsuari)
+                .addOnSuccessListener(aVoid -> Log.d(TAG, "DocumentSnapshot successfully written!"))
+                .addOnFailureListener(e -> {
+                    Log.w(TAG, "Error writing document", e);
+                    Toast toast2=Toast. makeText(getApplicationContext(),"No sha guardat a la BBDD",Toast. LENGTH_SHORT);
+                    toast2. setMargin(50,50);
+                    toast2. show();
+                });
+
+    }
+
+    private void GuardarUsuarioSalidaBBDD(long hora, long minuts) {
+
+        dadesUsuari.put("hora_sortida", hora);
+        dadesUsuari.put("minuts_sortida", minuts);
+
+        db.collection("users").document(Objects.requireNonNull(user.getEmail()))
+                .set(dadesUsuari)
+                .addOnSuccessListener(aVoid -> Log.d(TAG, "DocumentSnapshot successfully written!"))
+                .addOnFailureListener(e -> {
+                    Log.w(TAG, "Error writing document", e);
+                    Toast toast2=Toast. makeText(getApplicationContext(),"No sha guardat a la BBDD",Toast. LENGTH_SHORT);
+                    toast2. setMargin(50,50);
+                    toast2. show();
+                });
+
+    }
+
+
+
     private void comprovarEntradaSortida() {
 
-        Toast toast=Toast. makeText(getApplicationContext(),""+document.getData().get("hora_entrada"),Toast. LENGTH_SHORT);
+        Toast toast=Toast. makeText(getApplicationContext(),""+ Objects.requireNonNull(document.getData()).get("hora_entrada"),Toast. LENGTH_SHORT);
         toast. show();
 
-        long horaEntrada = (long) document.getData().get("hora_entrada");
-        long horaSortida = (long) document.getData().get("hora_sortida");
+        Map<String, Object> informacioUsuari = document.getData();
+
+        long horaEntrada = 0;
+        long horaSortida = 0;
+
+        try {
+            horaEntrada = (long) informacioUsuari.get("hora_entrada");
+        } catch (NullPointerException e) {
+            Log.d(TAG, "Error agafant hora d'entrada: " + e);
+        }
+
+        try {
+            horaSortida = (long) informacioUsuari.get("hora_sortida");
+        } catch (NullPointerException e) {
+            Log.d(TAG, "Error agafant hora de sortida: " + e);
+        }
 
         if (horaEntrada != 0) {
+
             iniciarTextView.setText(document.getData().get("hora_entrada") + ":" + document.getData().get("minuts_entrada"));
 
             if (horaSortida != 0) {
 
                 acabarTextView.setText(document.getData().get("hora_sortida") + ":" + document.getData().get("minuts_sortida"));
+                acabarJornadaBtn.setEnabled(false);
+
+            } else {
+
+                acabarJornadaBtn.setEnabled(true);
             }
 
+            iniciarJornadaBtn.setEnabled(false);
+
+        } else {
+            iniciarJornadaBtn.setEnabled(true);
         }
     }
 
-    public Map<String, Integer> getDataActual() {
+    public Map<String, Integer> getFechaActual() {
 
-        Map<String, Integer> data = new HashMap<>();
+        Map<String, Integer> fecha = new HashMap<>();
 
         int dia = Integer.parseInt(new SimpleDateFormat("dd", Locale.getDefault()).format(new Date()));
         int mes = Integer.parseInt(new SimpleDateFormat("MM", Locale.getDefault()).format(new Date()));
+        int anio = Integer.parseInt(new SimpleDateFormat("yyyy", Locale.getDefault()).format(new Date()));
         int hores = Integer.parseInt(new SimpleDateFormat("HH", Locale.getDefault()).format(new Date()));
         int minuts = Integer.parseInt(new SimpleDateFormat("mm", Locale.getDefault()).format(new Date()));
 
 
-        data.put("dia", dia);
-        data.put("mes", mes);
-        data.put("hores", hores);
-        data.put("minuts", minuts);
+        fecha.put("dia", dia);
+        fecha.put("mes", mes);
+        fecha.put("anio", anio);
+        fecha.put("hores", hores);
+        fecha.put("minuts", minuts);
 
-        return data;
+        return fecha;
 
     }
 
     public void iniciarJornada (View view) {
 
-        dataEntrada = getDataActual();
+        fechaEntrada = getFechaActual();
 
-        Map<String, Integer> horaEntrada = new HashMap<>();
-
-        horaEntrada.put("hora_entrada", dataEntrada.get("hores"));
-        horaEntrada.put("minuts_entrada", dataEntrada.get("minuts"));
-
-        db.collection("users").document(user.getEmail())
-                .set(horaEntrada)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Log.d(TAG, "DocumentSnapshot successfully written!");
-                        Toast toast=Toast. makeText(getApplicationContext(),"Actualitzada hora d'entrada",Toast. LENGTH_SHORT);
-                        toast. show();
-                        iniciarTextView.setText(dataEntrada.get("hores") + ":" + dataEntrada.get("minuts"));
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error writing document", e);
-                        Toast toast=Toast. makeText(getApplicationContext(),"No sha actualitzat a la BBDD",Toast. LENGTH_SHORT);
-                        toast. show();
-                    }
-                });
+        GuardarUsuarioEntradaBBDD(fechaEntrada.get("hores"), fechaEntrada.get("minuts"));
 
         iniciarJornadaBtn.setEnabled(false);
     }
 
     public void acabarJornada (View view) {
-        dataSortida = getDataActual();
+        fechaSortida = getFechaActual();
 
-        Toast toast=Toast. makeText(getApplicationContext(),dataSortida.get("dia") + "/" + dataSortida.get("mes"),Toast. LENGTH_SHORT);
-        toast. show();
+        GuardarUsuarioSalidaBBDD(fechaSortida.get("hores"), fechaSortida.get("minuts"));
 
-        acabarTextView.setText(dataSortida.get("hores") + ":" + dataSortida.get("minuts"));
         acabarJornadaBtn.setEnabled(false);
 
         calcularHores();
@@ -232,12 +249,14 @@ public class EmployeeActivity extends AppCompatActivity {
         iniciarJornadaBtn.setEnabled(true);
         acabarJornadaBtn.setEnabled(true);
 
+        
+        
     }
 
     private void calcularHores() {
 
-        int horasTotals = dataSortida.get("hores") - dataEntrada.get("hores");
-        int minutsTotals = dataSortida.get("minuts") - dataEntrada.get("minuts");
+        int horasTotals = fechaSortida.get("hores") - fechaEntrada.get("hores");
+        int minutsTotals = fechaSortida.get("minuts") - fechaEntrada.get("minuts");
 
         if (minutsTotals < 10)
             ((TextView) findViewById(R.id.resultat)).setText(new StringBuilder().append("Total: ").append(horasTotals + ":0" + minutsTotals).toString());
